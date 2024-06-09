86f32e67c23298ced14b946c8f93e7e3a4b52e2d
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-06-09 18:12:35 +0530

    com

---
+++ typeconversion.py
@@ -0,0 +1,20 @@
+print(type(10.5))
+x=10.5
+print(int(x))
+print(complex(x))
+print(bool(x))
+y=10+2j
+print(type(y))
+#print(int(y)) NOTE:complex data type cannot converted into float and integer datatype
+#print(float(y))
+print(bool(y))
+z=False
+print(type(z))
+print(int(z))
+print(float(z))
+print(complex(z))
+a=True
+print(type(a))
+print(int(a))
+print(float(a))
+print(complex(a))
