21eb19bfe93f30d5b027cec870825a439e17092a
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-06-13 08:26:16 +0530

    data

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+#question 1
+Institute="PALLE TECHNOLOGIES"
+#printing 'T' using positive and negative indexing
+print(Institute[6])
+print(Institute[-12])
+#printing 'G' 
+print(Institute[14])
+print(Institute[-4])
+#printing 'H'
+print(Institute[9])
+print(Institute[-9])
+#printing 'S'
+print(Institute[17])
+print(Institute[-1])
+
+#question 2
+stmt="I AM LEARNING PYTHON AT PALLE TECHNOLOGIES"
+print("LENGTH OG STRING:",len(stmt))
+print("printing using forward slicing")
+print(stmt[0])
+print(stmt[2:4])
+print(stmt[5:13])
+print(stmt[14:20])
+print(stmt[21:23])
+print(stmt[24:29])
+print(stmt[30:])
+print("printing using backward slicing")
+print(stmt[-42])
+print(stmt[-40:-38])
+print(stmt[-37:-29])
+print(stmt[-28:-22])
+print(stmt[-21:-19])
+print(stmt[-18:-13])
+print(stmt[-12:])
