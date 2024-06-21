3b6b02b527b9e42a40ce334ee4294859e1b15f64
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-06-21 04:43:05 +0000

    modify

--- fibonacci.py
+++ fibonacci.py
@@ -2,14 +2,17 @@ n=int(input("Enter Number :"))
 x=0
 y=1
 l=[]
+l1=[]
 sum=0
 while n!=0:
     print(x,end=" ")
     sum=sum+x
     l=l+[x]
+    l1=[x]+l1
     x,y=y,x+y
     n=n-1
 print("\nFibonacci in list:",l)
+print("Fibonacci in list in reverse:",l1)
 print("Sum of fibonacci series:",sum)
 print("Fibonacci series below 100:")
 x=0
@@ -17,7 +20,7 @@ y=1
 while x<100:
     print(x,end=" ")
     x,y=y,x+y
-print("All even Fibonacci numbers below 500:")
+print("\nAll even Fibonacci numbers below 500:")
 x=0
 y=1
 while x<500:
