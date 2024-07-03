8c4c7d4269e1e74def7608b57ed74a49debdda89
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-07-03 06:24:25 +0000

    str

---
+++ stringMethods.py
@@ -0,0 +1,19 @@
+str="krIshna is everything"
+print(str.upper())
+print(str.lower())
+print(str.title())#title case
+print(str.capitalize())# convert first character to uppercase
+print(str.swapcase())#swap the case
+print(str.count('i'))#character count
+print(str.count('e'))
+print(str.count('is'))
+print(str.endswith('thing'))
+print(str.endswith('everything'))
+print(str.endswith('is'))
+print(str.endswith('g',10,22))
+print(str.find('is'))#return first occurence of value,otherwise -1(same as index())
+print(str.find('god'))# -1
+
+
+
+
