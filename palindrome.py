4e9d60408e96e90f35b2da2a9f79fab1396ad8e0
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-07-17 05:17:01 +0000

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+num=int(input("Enter Number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if temp==rev:
+    print("Number is palindrome")
+else:
+    print("Number is not palindrome")
+    
