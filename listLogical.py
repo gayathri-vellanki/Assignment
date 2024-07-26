3e5fbdbb586288cab4ed197c9e7120208fc5631a
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-07-26 20:06:29 +0530

    explain

--- listLogical.py
+++ listLogical.py
@@ -11,3 +11,10 @@ for i in k:
 print(maxi)
 high=max(maxi)
 print(maxi.count(high))
+
+####
+# n=5
+# list=[1,2,1,2,3]
+# here, 1 appears 2times and 2 appears 2 times, and 3 1 time .hence count is[2,2,1]
+#here the maximum is 2 which appears 2 times .Hence output is 2.
+
