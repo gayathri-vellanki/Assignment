1c1667c7ebd6be073fed933d0c9f76f739596275
gayathri-vellanki <100945822+gayathri-vellanki@users.noreply.github.com>
2024-08-03 07:40:51 +0530

    fact

--- factorial.py
+++ factorial.py
@@ -4,3 +4,14 @@ for i in range(1,num+1):
     fact=fact*i
 print("facatorial of number:",fact)
 
+num=int(input("enter number to find range of factorials:"))
+for i in range(1,num+1):
+    fact=1
+    if i==0 or i==1:
+        print("1",end=" ")
+    else:
+        for j in range(2,i+1):
+            fact=fact*j
+        print(fact,end=" ")
+            
+    
